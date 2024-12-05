8295c364e9ec8565ed7649ae0533c47d8ed94b50
glk1001 <gregg.kay@gmail.com>
2024-12-05 22:15:18 +1100

    Moved date checking and work_dir functions over from barks-compleat.

--- src/barks_fantagraphics/comics_utils.py
+++ src/barks_fantagraphics/comics_utils.py
@@ -1,8 +1,20 @@
 import os
+from datetime import datetime
 from pathlib import Path
 from typing import Union
 
 from .comics_consts import BARKS_ROOT_DIR
+
+
+def get_work_dir(work_dir_root: str) -> str:
+    os.makedirs(work_dir_root, exist_ok=True)
+    if not os.path.isdir(work_dir_root):
+        raise Exception(f'Could not find work root directory "{work_dir_root}".')
+
+    work_dir = os.path.join(work_dir_root, datetime.now().strftime("%Y_%m_%d-%H_%M_%S.%f"))
+    os.makedirs(work_dir)
+
+    return work_dir
 
 
 def get_relpath(file: Union[str, Path]) -> str:
@@ -15,3 +27,35 @@ def get_relpath(file: Union[str, Path]) -> str:
 
 def get_clean_path(file: Union[str, Path]) -> str:
     return str(file).replace(str(Path.home()), "$HOME")
+
+
+def get_timestamp(file: str) -> float:
+    if os.path.islink(file):
+        return os.lstat(file).st_mtime
+
+    return os.path.getmtime(file)
+
+
+def get_timestamp_str(file: str) -> str:
+    return get_timestamp_as_str(get_timestamp(file))
+
+
+def get_timestamp_as_str(timestamp: float) -> str:
+    timestamp_as_date = datetime.fromtimestamp(timestamp)
+    return timestamp_as_date.strftime("%Y_%m_%d-%H_%M_%S.%f")
+
+
+def dest_file_is_older_than_srce(srce_file: str, dest_file: str, include_missing_dest=True) -> bool:
+    if include_missing_dest and not os.path.exists(dest_file):
+        return True
+
+    srce_timestamp = get_timestamp(srce_file)
+    dest_timestamp = get_timestamp(dest_file)
+
+    return srce_timestamp > dest_timestamp
+
+
+def file_is_older_than_timestamp(file: str, timestamp: float) -> bool:
+    file_timestamp = get_timestamp(file)
+
+    return file_timestamp > timestamp
