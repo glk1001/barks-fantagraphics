0aba1eb90899a33f3e631081228cbe0f890c1310
glk1001 <gregg.kay@gmail.com>
2024-12-09 23:12:25 +1100

    Improved command line argument handling.

--- src/barks_fantagraphics/comics_cmd_args.py
+++ src/barks_fantagraphics/comics_cmd_args.py
@@ -1,50 +1,150 @@
 import argparse
+from enum import Flag, auto
+from typing import List, Tuple
 
-from .comics_database import get_default_comics_database_dir
+from intspan import intspan
+
+from .comics_database import ComicsDatabase, get_default_comics_database_dir
 
 COMICS_DATABASE_DIR_ARG = "--comics-database-dir"
 VOLUME_ARG = "--volume"
 TITLE_ARG = "--title"
+PAGE_ARG = "--page"
 
 
-def get_std_args():
-    parser = argparse.ArgumentParser(
-        description="Upscayl jpg images in specified Fantagraphics volumes."
-    )
-
-    parser.add_argument(
-        COMICS_DATABASE_DIR_ARG,
-        action="store",
-        type=str,
-        default=get_default_comics_database_dir(),
-    )
-    parser.add_argument(
-        VOLUME_ARG,
-        action="store",
-        type=str,
-        required=False,
-    )
-    parser.add_argument(
-        TITLE_ARG,
-        action="store",
-        type=str,
-        required=False,
-    )
-
-    args = parser.parse_args()
-
-    if not _validate(args):
-        return None
-
-    return args
+class CmdArgNames(Flag):
+    COMICS_DATABASE_DIR = auto()
+    VOLUME = auto()
+    TITLE = auto()
+    PAGE = auto()
 
 
-def _validate(args) -> bool:
-    if args.volume and args.title:
-        print(f"ERROR: You can only have one of '{VOLUME_ARG}' or '{TITLE_ARG}'.")
-        return False
-    if args.volume is None and args.title is None:
-        print(f"ERROR: You must specify one of '{VOLUME_ARG}' or '{TITLE_ARG}'.")
-        return False
+class CmdArgs:
+    def __init__(
+        self, description: str, required_args: CmdArgNames = CmdArgNames.COMICS_DATABASE_DIR
+    ):
+        self._description = description
+        self._required_args = required_args
+        self._error_msg = ""
+        self._cmd_args = self._get_args()
+        self._comics_database = ComicsDatabase(self._cmd_args.comics_database_dir)
 
-    return True
+    def args_are_valid(self) -> Tuple[bool, str]:
+        if not self._error_msg:
+            return True, ""
+        return False, self._error_msg
+
+    def get_comics_database(self) -> ComicsDatabase:
+        return self._comics_database
+
+    def get_title(self) -> str:
+        if CmdArgNames.TITLE not in self._required_args:
+            raise Exception(f"'{TITLE_ARG}' was not specified as an argument.")
+        return self._cmd_args.title
+
+    def get_titles(self) -> List[str]:
+        if (
+            CmdArgNames.TITLE not in self._required_args
+            and CmdArgNames.VOLUME not in self._required_args
+        ):
+            raise Exception(
+                f"One of '{TITLE_ARG}' or '{VOLUME_ARG}' were not specified as an argument."
+            )
+
+        if self._cmd_args.title:
+            return [self._cmd_args.title]
+
+        assert self._cmd_args.volume is not None
+        vol_list = list(intspan(self._cmd_args.volume))
+        return self._comics_database.get_all_story_titles_in_fantagraphics_volume(vol_list)
+
+    def get_volume(self) -> str:
+        volumes = self.get_volumes()
+        if len(volumes) > 1:
+            raise Exception(f"'{VOLUME_ARG}' specified more than one volume.")
+
+        return volumes[0]
+
+    def get_volumes(self) -> List[str]:
+        if CmdArgNames.VOLUME not in self._required_args:
+            raise Exception(f"'{VOLUME_ARG}' was not specified as an argument.")
+
+        assert self._cmd_args.volume is not None
+        return list(intspan(self._cmd_args.volume))
+
+    def get_pages(self) -> List[str]:
+        if CmdArgNames.PAGE not in self._required_args:
+            raise Exception(f"'{PAGE_ARG}' was not specified as an argument.")
+
+        assert self._cmd_args.page is not None
+        return list(intspan(self._cmd_args.page))
+
+    def _get_args(self):
+        parser = argparse.ArgumentParser(description=self._description)
+
+        parser.add_argument(
+            COMICS_DATABASE_DIR_ARG,
+            action="store",
+            type=str,
+            default=get_default_comics_database_dir(),
+        )
+        parser.add_argument(
+            VOLUME_ARG,
+            action="store",
+            type=str,
+            required=False,
+        )
+        parser.add_argument(
+            TITLE_ARG,
+            action="store",
+            type=str,
+            required=False,
+        )
+        parser.add_argument(
+            PAGE_ARG,
+            action="store",
+            type=str,
+            required=False,
+        )
+
+        args = parser.parse_args()
+
+        self._validate(args)
+
+        return args
+
+    def _validate(self, args) -> None:
+        if CmdArgNames.VOLUME | CmdArgNames.TITLE == self._required_args:
+            if not args.volume and not args.title:
+                self._error_msg = f"ERROR: You must specify one of '{VOLUME_ARG}' or '{TITLE_ARG}."
+                return
+            if args.volume and args.title:
+                self._error_msg = (
+                    f"ERROR: You must specify only one of '{VOLUME_ARG}' or '{TITLE_ARG}."
+                )
+                return
+            return
+
+        if args.volume and args.page:
+            self._error_msg = f"ERROR: You cannot specify '{PAGE_ARG}' with '{VOLUME_ARG}'."
+            return
+
+        if CmdArgNames.VOLUME in self._required_args and not args.volume:
+            self._error_msg = f"ERROR: You must specify a '{VOLUME_ARG}' argument."
+            return
+        if CmdArgNames.TITLE in self._required_args and not args.title:
+            self._error_msg = f"ERROR: You must specify a '{TITLE_ARG}' argument."
+            return
+        if CmdArgNames.PAGE in self._required_args and not args.page:
+            self._error_msg = f"ERROR: You must specify a '{PAGE_ARG}' argument."
+            return
+
+        if CmdArgNames.VOLUME not in self._required_args and args.volume:
+            self._error_msg = f"ERROR: Unexpected argument: '{VOLUME_ARG}'."
+            return
+        if CmdArgNames.TITLE not in self._required_args and args.title:
+            self._error_msg = f"ERROR: Unexpected argument: '{TITLE_ARG}'."
+            return
+        if CmdArgNames.PAGE not in self._required_args and args.page:
+            self._error_msg = f"ERROR: Unexpected argument: '{PAGE_ARG}'."
+            return
